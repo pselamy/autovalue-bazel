c944f07af7b2b0616591ab080c799f403d38b0c6
Patrick Selamy <pselamy@gmail.com>
2022-09-03 21:45:44 -0400

    Update AutoValueTest.java

--- src/test/java/com/github/pselamy/AutoValueTest.java
+++ src/test/java/com/github/pselamy/AutoValueTest.java
@@ -21,15 +21,4 @@ public class AutoValueTest {
     assertThat(actual.foo()).equals(FOO);
     assertThat(actual.bar()).equals(BAR);
   }
-  
-  @AutoValue
-  abstract static class ObjectUnderTest {
-    private static ObjectUnderTest create(String foo, int bar) {
-      return new AutoValueTest_ObjectUnderTest(foo, bar);
-    }
-    
-    abstract String foo();
-    
-    abstract int bar();
-  }
 }
